f17ccc8a2844c0b5895803ca89e6f57d72439069
Gabe Lapingcao <mgultra2018@gmail.com>
2026-07-10 00:43:13 -0700

    WIP: reformat using clang format

--- tests/gtest/src/test_bgp_ls_edge_update.cpp
+++ tests/gtest/src/test_bgp_ls_edge_update.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
-{
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_STRNE("hello", "world");
+  EXPECT_EQ(7 * 6, 42);
 }
